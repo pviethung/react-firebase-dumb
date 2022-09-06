e09e1768d8393ad336fa99174e0cd0fdc7cf9b66
pviethung <pv2hung97@gmail.com>
2022-09-06 16:24:44 +0700

    add app routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Routes>
-      <Route path="/"></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }

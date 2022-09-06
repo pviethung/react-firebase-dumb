4718147233cba7c6305cfd6b5ca4dd35936811c8
pviethung <pv2hung97@gmail.com>
2022-09-06 17:09:55 +0700

    add global styles and Navbar style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={`${styles.container}`}>
+      <ul className="container">
+        <li>MyMoney</li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/singup">Signup</Link>
+        </li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

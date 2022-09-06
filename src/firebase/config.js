00fd6010a24e8dd80fc9632bd8ee7630507e9bd2
pviethung <pv2hung97@gmail.com>
2022-09-06 16:31:25 +0700

    add firestore and firebase auth

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDaXmtMxIASnNrSLfR0sd-cViHG23aAniQ',
+  authDomain: 'auth-and-db-6dfbb.firebaseapp.com',
+  projectId: 'auth-and-db-6dfbb',
+  storageBucket: 'auth-and-db-6dfbb.appspot.com',
+  messagingSenderId: '968590275559',
+  appId: '1:968590275559:web:13c129b7c45ebcf9f40342',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// firestore
+export const db = getFirestore(app);
+
+// auth
+export const auth = getAuth(app);
